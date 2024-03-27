e6c14433a0b8eee95784ebc4a8489d6748cbc71c
oofytronic <oof@motualis.com>
2024-03-27 07:06:55 -0400

    fehwio

--- skydash/ui.js
+++ skydash/ui.js
@@ -1,19 +1,4 @@
 import {capitalize, truncateString, applyMarkdown, fileToDataUrl} from './utilities.js';
-import { createHelia } from 'helia'
-
-// create a Helia node
-const helia = await createHelia();
-
-async function publishToIPFS(jsonData) {
-  // Add JSON data to IPFS
-  const { cid } = await ipfs.add(JSON.stringify(jsonData));
-
-  // Update IPNS key with new CID
-  const ipnsKey = 'k51qzi5uqu5dh4bzmotfzj2qezrs1hbrkogs3zb7olevc6z51kccnrtdcoy55l';
-  const { name } = await ipfs.name.publish(cid.toString(), { key: ipnsKey });
-
-  return name;
-}
 
 
 // MAIN
